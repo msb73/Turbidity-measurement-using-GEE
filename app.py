798c4655feda10146f84f4298a75fbc5e290ea4e
Yogesh Kunkawalekar <43066840+yogeshkun@users.noreply.github.com>
2023-04-14 03:31:41 +0530

    UI / Routing
    Added calendar UI and location dropdown and pass value to /submitdata route inside app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # Run application using python app.run
-from flask import Flask, render_template
+from flask import Flask, render_template,  request, jsonify
 import folium
 from folium.plugins import Draw, Search
 import ee
@@ -13,6 +13,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route('/submitdata', methods=['POST'])
+def submit_data():
+    if request.method == 'POST':
+        data = request.get_json()  # get the JSON data from the request body
+        # do something with the data, e.g. store it in a database
+        print(data)  # print the data to the console
+        return jsonify({'success': True}), 200  # return a success response
+    else:
+        return jsonify({'error': 'Invalid request method'}), 405  # return an error response if the request method is not POST
 
 if __name__ == '__main__':
     app.run()
