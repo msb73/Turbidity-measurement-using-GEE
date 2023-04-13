2d10865e4417d71ea283db627df27f6a49840eff
Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
2023-04-13 05:45:59 +0530

    Added Markers

--- fol.py
+++ fol.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 import folium
-from folium.plugins import Draw, Search
+from folium import plugins
 import ee
 from basemap import basemaps
 
@@ -12,25 +12,47 @@ def fullscreen():
         'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
     
     my_map = folium.Map(location=[18.41, 73.74], zoom_start=12, tiles="Stamen Terrain")
-    tooltip = "Click me!"
+    #tooltip = "Click me!"
 
-    folium.Marker(
-    [18.41, 73.74], popup="<i>Khadakwasla</i>", tooltip=tooltip
-    ).add_to(my_map)
+    # folium.Marker(
+    # [18.41, 73.74], popup="<i>Khadakwasla</i>", tooltip=tooltip
+    # ).add_to(my_map)
 
-    folium.Marker(
-    [18.4323, 73.7624], popup="<i>Khadakwasla</i>", tooltip=tooltip
-    ).add_to(my_map)
+    # folium.Marker(
+    # [18.4323, 73.7624], popup="<i>Khadakwasla</i>", tooltip=tooltip
+    # ).add_to(my_map)
 
-    folium.Marker(
-    [18.3946, 73.7022], popup="<i>Khadakwasla</i>", tooltip=tooltip
-    ).add_to(my_map)
+    # folium.Marker(
+    # [18.3946, 73.7022], popup="<i>Khadakwasla</i>", tooltip=tooltip
+    # ).add_to(my_map)
 
+    draw_data = plugins.Draw(export=False,position='topleft', draw_options={'marker': True, 'polyline': False, 
+                                                                 'polygon': False,
+                                                                 'rectangle': False,
+                                                                 'circle': False,
+                                                                 'circlemarker': False}, edit_options={'edit': False})  
+    
+    draw_data.add_to(my_map)
+    
+    
+    # folium.Marker(
+    # [18.3964, 73.6724], popup="<i>Khadakwasla</i>", tooltip=tooltip
+    # ).add_to(my_map)
+    ### GRAPHS CODE START
+    import json
+    import requests
+
+    url = (
+        "https://raw.githubusercontent.com/python-visualization/folium/main/examples/data"
+    )
+    vis3 = json.loads(requests.get(f"{url}/vis2.json").text)
     folium.Marker(
-    [18.3964, 73.6724], popup="<i>Khadakwasla</i>", tooltip=tooltip
+    location=[18.3964, 73.672],
+    popup=folium.Popup(max_width=450).add_child(
+        folium.Vega(vis3, width=450, height=250)
+    ),
     ).add_to(my_map)
-    # folium.Marker(location =[data.iloc[i]['lat'], data.iloc[i]['lon']], 
-    #               popup =data.iloc[i]['name'], icon = folium.Icon(color ='red')).add_to(n)
+    ### GRAPH CODE END
 
     basemaps['Google Maps'].add_to(my_map)
     basemaps['Google Satellite Hybrid'].add_to(my_map)
