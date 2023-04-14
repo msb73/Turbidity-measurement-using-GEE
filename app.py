0d279b4cb737c751146318c3e8a98586f8b1b947
Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
2023-04-14 21:41:34 +0530

    Revert "Merge remote-tracking branch 'origin/dev_addMarkers' into dev_add_layers"
    This reverts commit 9bcc0e19e48be0515a42b3608d13545668366581, reversing
    changes made to a19261911e135e9ed77b42bb30ef0f04c7d1af4d.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-# Run application using python app.run
-from flask import Flask, render_template,  request, jsonify
+from flask import Flask
 import folium
-import ee
+from authenticate import ee
+from imageCollection import imageCollection
+from gee import gee
 from basemap import basemaps
 from cloud_mask import maskS2clouds
 # def authenticate():
@@ -14,14 +15,16 @@ app = Flask(__name__)
 def fullscreen():
     """Simple example of a fullscreen map."""
     m = folium.Map()
-    vis_params = {
-        'min': 0,
-        'max': 4000,
-        'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']}
+    # vis_params = {
+    #     'min': 0,
+    #     'max': 4000,
+    #     'palette': ['006633', 'E5FFCC', '662A00', 'D8D8D8', 'F5F5F5']
+    #     }
 
 # Create a folium map object.
     my_map = folium.Map(location=[20, 0], zoom_start=3, height=500)
     basemaps['Google Maps'].add_to(my_map)
     basemaps['Google Satellite Hybrid'].add_to(my_map)
+    gee(my_map)
     my_map.add_child(folium.LayerControl())
     return my_map.get_root().render()
