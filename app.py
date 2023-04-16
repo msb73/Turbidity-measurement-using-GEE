6b6c8ec112b4c1e33eb4598449e39b2e1f57a7e5
Yogesh Kunkawalekar <43066840+yogeshkun@users.noreply.github.com>
2023-04-16 18:54:12 +0530

    Squashed commit of the following:
    commit c5675d1dae01dcee08862a9a76b7246f035cb056
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Sun Apr 16 13:44:45 2023 +0530
    
        Revert "Minor changes"
    
        This reverts commit 866b25501bcc11e9d2ef3583045d723714c84bf6.
    
    commit 866b25501bcc11e9d2ef3583045d723714c84bf6
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Sun Apr 16 13:20:18 2023 +0530
    
        Minor changes
    
    commit 24983e2011adef492296960a4e4804cdb4c76fa3
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Sat Apr 15 13:18:46 2023 +0530
    
        NDTI layer added
    
    commit e6b2bf56e271b1e94e04398c63dfd36fe0a014ce
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Fri Apr 14 13:24:07 2023 +0530
    
        working on layers.py
    
    commit 528cc16d3c15f7befedb1d9fb549b86bc8f8b56e
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Sat Apr 15 19:48:44 2023 +0530
    
        created requirements.txt
    
    commit 53bdeb078bc9aed9f619ed880821e3e1a5044710
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Sat Apr 15 19:10:23 2023 +0530
    
        Added line chart and bar chart code
    
    commit 0a2e65d05059720646bee3fecdf12df497b3a78c
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 21:53:46 2023 +0530
    
        Revert "Visualised the data"
    
        This reverts commit 235f205c74f20a35499b56e2366b0b6fdb59639b.
    
    commit 0d279b4cb737c751146318c3e8a98586f8b1b947
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 21:41:34 2023 +0530
    
        Revert "Merge remote-tracking branch 'origin/dev_addMarkers' into dev_add_layers"
    
        This reverts commit 9bcc0e19e48be0515a42b3608d13545668366581, reversing
        changes made to a19261911e135e9ed77b42bb30ef0f04c7d1af4d.
    
    commit 9bcc0e19e48be0515a42b3608d13545668366581
    Merge: a192619 e149895
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 21:40:31 2023 +0530
    
        Merge remote-tracking branch 'origin/dev_addMarkers' into dev_add_layers
    
    commit a19261911e135e9ed77b42bb30ef0f04c7d1af4d
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Fri Apr 14 09:55:16 2023 +0530
    
        minor changes
    
    commit 235f205c74f20a35499b56e2366b0b6fdb59639b
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 08:05:06 2023 +0530
    
        Visualised the data
    
        visualised the data and saved the graphs in /graphs folder
    
    commit 13241b4c87d1a5aaaba53bf81a3e9a1a24b0f1fc
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 07:52:56 2023 +0530
    
        single turbidity value for each date/key in dic
    
    commit 9c92c1b5f706feb5539df36cf7daedb2002ce5f8
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Fri Apr 14 07:48:47 2023 +0530
    
        Get turbidity values from rgb values
    
    commit 8ee8a1cb0fafdc83d9113df4b8f71d1141224468
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Thu Apr 13 15:34:26 2023 +0530
    
        length of dic
    
    commit c6ca5038941f05574d52e7e2a36bc078fdb96afb
    Author: Sanjyot <38827347+Sanjyot789@users.noreply.github.com>
    Date:   Thu Apr 13 15:29:41 2023 +0530
    
        printed dictionary
    
    commit 43fa3c13b53bc334cbc37b08a7cab622eaa71047
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Thu Apr 13 15:26:11 2023 +0530
    
        Minor change
    
    commit 11557d7179965d6f4d82c09b5abb15880cbcf52b
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Thu Apr 13 14:36:41 2023 +0530
    
        Feature: retrieve rgb values for point;  type : raw   ; additional : convert to {date:[values]} dict
    
    commit bab266d48dfe3909d34f3efeac9563ac34648e40
    Author: Milind Bharambe <milindbharambe7@gmail.com>
    Date:   Thu Apr 13 06:08:43 2023 +0530
    
        added temperory raster_layer

--- app.py
+++ app.py
@@ -1,41 +1,46 @@
-# Run application using python app.run
-from flask import Flask, render_template,  request, jsonify
+from flask import Flask, request
 import folium
-from folium.plugins import Draw, Search
-import ee
+from authenticate import ee
+from imageCollection import imageCollection
 from basemap import basemaps
+import layers
+
+
 # def authenticate():
 #     ee.Authenticate()
 #     ee.Initialize()
 # authenticate()
 app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-
-@app.route('/submitdata', methods=['POST'])
-def submit_data():
+collection = ee.ImageCollection
+@app.route("/", methods = ['POST', 'GET'])
+def fullscreen():
+    my_map = folium.Map(location=[20, 0], zoom_start=3, height=1000)
+    basemaps['Google Satellite Hybrid'].add_to(my_map)
+    # my_map.add_child(folium.LayerControl())
     if request.method == 'POST':
-        data = request.get_json()  # get the JSON data from the request body
-        # do something with the data, e.g. store it in a database
-        print(data)  # print the data to the console
-        return jsonify({'success': True}), 200  # return a success response
-    else:
-        return jsonify({'error': 'Invalid request method'}), 405  # return an error response if the request method is not POST
-
-@app.route('/getCoordinates', methods=['POST'])
-def get_Coordinates():
-    if request.method == 'POST':
-        Coordinates = request.get_json()  # get the JSON data from the request body
-        # do something with the data, e.g. store it in a database
-        print(Coordinates)  # print the data to the console
-        return jsonify({'success': True}), 200  # return a success response
-    else:
-        return jsonify({'error': 'Invalid request method'}), 405  # return an error response if the request method is not POST
-
-
-if __name__ == '__main__':
-    app.run()
+        print('post')
+        return my_map.get_root().render()
+    date = ('2023-01-01', '2023-02-15')
+    location = 'Khadakwasla'
+    # locations = {
+    #     'Khadakwasla' : ([20, 0], ee.Geometry.Point([[73.73801385248593, 18.412317318279012]]), 3),
+    #     'Mula mutha' : ([20, 0], ee.Geometry.Point([[73.8417693187197, 18.436935449462606]]), 30),
+    #     'Jambhulwadi' : ([20, 0], ee.Geometry.Point([[73.85916092299954, 18.526407602714407]]), 3)
+    # }
+    
+    collection = imageCollection(date)
+    visParams = {'min':0, 'max':1, 
+                 'opacity' : 1,
+             'palette':['red','blue']
+             }
+    # layers.addRasterLayers(collection.select(['B2', 'B3', 'B4']).mean(), my_map, 'gotit', visParams)
+    collection = layers.ndti(collection, my_map, location)
+    print('app')
+    print(id(my_map))
+    print(dir(my_map.add_to))
+    print(my_map.add_to)
+    #get center at Khadakwasla
+    my_map.add_child(folium.LayerControl())
+    print('get')
+    return my_map.get_root().render()
 
